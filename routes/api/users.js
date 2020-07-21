7b9ae1ff32a1b7ad8dec5a667c41ecfaac6b43ca
scrumpygeo <scrumpygeo@yahoo.ca>
2020-07-21 11:44:25 +0100

    registration validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
 
-// @route   GET api/users
-// @desc    Test Route
+// @route   POST api/users
+// @desc    Register user
 // @access: Public
-router.get('/', (req, res) => res.send(res.body));
+router.post(
+  '/',
+  [
+    check('name', 'Please include a name').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Pleas enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    // if there are errors return status 400 and send array of error messages
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // See if user exists
+
+    // Get user's gravatar
+
+    // Encrypt pwd
+
+    // Return jwt  - so when user registers he's logged in right away
+
+    res.send('User route');
+  }
+);
 
 module.exports = router;
