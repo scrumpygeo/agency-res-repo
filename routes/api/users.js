7c3bee965a379f725847550453458eba27f253a4
scrumpygeo <scrumpygeo@yahoo.ca>
2020-07-21 09:47:23 +0100

    add user model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../../models/User');
+
 // @route   GET api/users
 // @desc    Test Route
 // @access: Public
-router.get('/', (req, res) => res.send('Users route'));
+router.get('/', (req, res) => res.send('Regiser a user'));
 
 module.exports = router;
