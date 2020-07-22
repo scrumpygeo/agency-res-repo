1f67eccf92346f54ba72a029ac00afc5f73d7115
scrumpygeo <scrumpygeo@yahoo.ca>
2020-07-22 17:36:48 +0100

    add experience to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -169,4 +169,62 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/profiles/experience
+// @desc    Add profile experience
+// @access: Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    // create object with data user submits:
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    // now deal with mongodb:
+    try {
+      // 1st get profile u want to add experience to:
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+      //
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
